b4cf6e8c75ac7e03c6b319a71a465ffbe6d76b73
rezaa1079 <80579563+rezaa1079@users.noreply.github.com>
2023-01-07 02:41:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+var a =Number(prompt("Enter n"));
+console.log("n = " + a)
+
+var b=Math.floor(Math.sqrt(a))
+ console.log("r =" + b);
+
+
+var x1=b+1
+var y1=Math.pow(x1,2)-a
+var z1=x1-Math.sqrt(y1)
+var t1=x1+Math.sqrt(y1)
+// ------------------------------
+var x2=b+2
+var y2=Math.pow(x2,2)-a
+var z2=x2-Math.sqrt(y2)
+var t2=x2+Math.sqrt(y2)
+// ------------------------------
+var x3=b+3
+var y3=Math.pow(x3,2)-a
+var z3=x3-Math.sqrt(y3)
+var t3=x3+Math.sqrt(y3)
+// ------------------------------
+var x4=b+4
+var y4=Math.pow(x4,2)-a
+var z4=x4-Math.sqrt(y4)
+var t4=x4+Math.sqrt(y4)
+// ------------------------------
+var x5=b+5
+var y5=Math.pow(x5,2)-a
+var z5=x5-Math.sqrt(y5)
+var t5=x5+Math.sqrt(y5)
+// ------------------------------
+var x6=b+6
+var y6=Math.pow(x6,2)-a
+var z6=x6-Math.sqrt(y6)
+var t6=x6+Math.sqrt(y6)
+// ------------------------------
+var x7=b+7
+var y7=Math.pow(x7,2)-a
+var z7=x7-Math.sqrt(y7)
+var t7=x7+Math.sqrt(y7)
+// ------------------------------
+var x8=b+8
+var y8=Math.pow(x8,2)-a
+var z8=x8-Math.sqrt(y8)
+var t8=x8+Math.sqrt(y8)
+// ------------------------------
+var x9=b+9
+var y9=Math.pow(x9,2)-a
+var z9=x9-Math.sqrt(y9)
+var t9=x9+Math.sqrt(y9)
+// ------------------------------
+var x10=b+10
+var y10=Math.pow(x10,2)-a
+var z10=x10-Math.sqrt(y10)
+var t10=x10+Math.sqrt(y10)
+// ------------------------------
+
+
+if (Number.isInteger(Math.sqrt(y1))) 
+{
+  console.log("i=1" + "Then x is r +1")
+  console.log("x^2 - n =" + y1)
+  console.log(y1 + " is Sqrtable then " + y1 + "=" + Math.sqrt(y1) + "*" + Math.sqrt(y1))
+  console.log("n = " +x1+"^2" + " - " + Math.sqrt(y1)+"^2")
+  console.log("n = " + x1 + "-" + Math.sqrt(y1) + " * " + x1 + "+" + Math.sqrt(y1))
+  console.log("n = " + z1 + "*" + t1)
+}
+else if(Number.isInteger(Math.sqrt(y2)))
+{
+  console.log("i=2" + "Then x is r +2")
+  console.log("x^2 - n =" + y2)
+  console.log(y2 + " is Sqrtable then " + y2 + "=" + Math.sqrt(y2) + "*" + Math.sqrt(y2))
+  console.log("n = " +x2+"^2" + " - " + Math.sqrt(y2)+"^2")
+  console.log("n = " + x2 + "-" + Math.sqrt(y2) + " * " + x2 + "+" + Math.sqrt(y2))
+  console.log("n = " + z2 + "*" + t2)
+}
+else if (Number.isInteger(Math.sqrt(y3)))
+{
+  console.log("i=3" + "Then x is r +3")
+  console.log("x^2 - n =" + y3)
+  console.log(y3 + " is Sqrtable then " + y3 + "=" + Math.sqrt(y3) + "*" + Math.sqrt(y3))
+  console.log("n = " +x3+"^2" + " - " + Math.sqrt(y3)+"^2")
+  console.log("n = " + x3 + "-" + Math.sqrt(y3) + " * " + x3 + "+" + Math.sqrt(y3))
+  console.log("n = " + z3 + "*" + t3)
+}
+else if (Number.isInteger(Math.sqrt(y4)))
+{
+  console.log("i=4" + "Then x is r +4")
+  console.log("x^2 - n =" + y4)
+  console.log(y4 + " is Sqrtable then " + y4 + "=" + Math.sqrt(y4) + "*" + Math.sqrt(y4))
+  console.log("n = " +x4+"^2" + " - " + Math.sqrt(y4)+"^2")
+  console.log("n = " + x4 + "-" + Math.sqrt(y4) + " * " + x4 + "+" + Math.sqrt(y4))
+  console.log("n = " + z4 + "*" + t4)
+}
+else if (Number.isInteger(Math.sqrt(y5)))
+{
+  console.log("i=5" + "Then x is r +5")
+  console.log("x^2 - n =" + y5)
+  console.log(y5 + " is Sqrtable then " + y5 + "=" + Math.sqrt(y5) + "*" + Math.sqrt(y5))
+  console.log("n = " +x5+"^2" + " - " + Math.sqrt(y5)+"^2")
+  console.log("n = " + x5 + "-" + Math.sqrt(y5) + " * " + x5 + "+" + Math.sqrt(y5))
+  console.log("n = " + z5 + "*" + t5)
+}
+else if (Number.isInteger(Math.sqrt(y6)))
+{
+  console.log("i=6" + "Then x is r +6")
+  console.log("x^2 - n =" + y6)
+  console.log(y6 + " is Sqrtable then " + y6 + "=" + Math.sqrt(y6) + "*" + Math.sqrt(y6))
+  console.log("n = " +x6+"^2" + " - " + Math.sqrt(y6)+"^2")
+  console.log("n = " + x6 + "-" + Math.sqrt(y6) + " * " + x6 + "+" + Math.sqrt(y6))
+  console.log("n = " + z6 + "*" + t6)
+}
+else if (Number.isInteger(Math.sqrt(y7)))
+{
+  console.log("i=7" + "Then x is r +7")
+  console.log("x^2 - n =" + y7)
+  console.log(y7 + " is Sqrtable then " + y7 + "=" + Math.sqrt(y7) + "*" + Math.sqrt(y7))
+  console.log("n = " +x7+"^2" + " - " + Math.sqrt(y7)+"^2")
+  console.log("n = " + x7 + "-" + Math.sqrt(y7) + " * " + x7 + "+" + Math.sqrt(y7))
+  console.log("n = " + z7 + "*" + t7)
+}
+else if (Number.isInteger(Math.sqrt(y8)))
+{
+  console.log("i=8" + "Then x is r +8")
+  console.log("x^2 - n =" + y8)
+  console.log(y8 + " is Sqrtable then " + y8 + "=" + Math.sqrt(y8) + "*" + Math.sqrt(y8))
+  console.log("n = " +x8+"^2" + " - " + Math.sqrt(y8)+"^2")
+  console.log("n = " + x8 + "-" + Math.sqrt(y8) + " * " + x8 + "+" + Math.sqrt(y8))
+  console.log("n = " + z8 + "*" + t8)
+}
+else if (Number.isInteger(Math.sqrt(y9)))
+{
+  console.log("i=9" + "Then x is r +9")
+  console.log("x^2 - n =" + y9)
+  console.log(y9 + " is Sqrtable then " + y9 + "=" + Math.sqrt(y9) + "*" + Math.sqrt(y9))
+  console.log("n = " +x9+"^2" + " - " + Math.sqrt(y9)+"^2")
+  console.log("n = " + x9 + "-" + Math.sqrt(y9) + " * " + x9 + "+" + Math.sqrt(y9))
+  console.log("n = " + z9 + "*" + t9)
+}
+else if (Number.isInteger(Math.sqrt(y10)))
+{
+  console.log("i=10" + "Then x is r +10")
+  console.log("x^2 - n =" + y10)
+  console.log(y10 + " is Sqrtable then " + y10 + "=" + Math.sqrt(y10) + "*" + Math.sqrt(y10))
+  console.log("n = " +x10+"^2" + " - " + Math.sqrt(y10)+"^2")
+  console.log("n = " + x10 + "-" + Math.sqrt(y10) + " * " + x10 + "+" + Math.sqrt(y10))
+  console.log("n = " + z10 + "*" + t10)
+}
+
+
+
+
